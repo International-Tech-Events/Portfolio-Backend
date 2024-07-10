081d5c1bed2c6852ee76ec7405d9791ef2d753de
giftyamoakowaa <maameamoakowaa1@gmail.com>
2024-07-10 09:07:18 +0000

    userSchema

---
+++ schema/user_schema.js
@@ -0,0 +1,14 @@
+import Joi from "joi";
+
+export const userSchema = joi.object({
+    user: {
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        otherNmaes: Joi.string(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(4).required(),
+        confirmedPassword: Joi.ref('password'),
+        userName: Joi.string(),
+        termsAndCondition: Joi.boolean(),
+    }
+})
