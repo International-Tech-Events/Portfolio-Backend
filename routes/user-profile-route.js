729e81cbe91f6431f152f74bc0f5de5ad67566a0
giftyamoakowaa <maameamoakowaa1@gmail.com>
2024-07-15 17:12:42 +0000

    New model changes

--- routes/user-profile-route.js
+++ routes/user-profile-route.js
@@ -1,14 +1,37 @@
-// import { Router } from "express";
-// import { postuserProfile, getUserProfile, getUserProfileById,updateUserProfile, deleteUserProfile} from "../controllers/user-profile-controller";
-// import { checkUserSession } from "../middlewares/auth";
+import { Router } from "express";
+import { createUserProfile,getUserProfile, updateUserProfile} from "../controllers/user-profile-controller.js";
+import { checkUserSession } from "../middlewares/auth.js";
+import { remoteUpload } from "../middlewares/uploads.js";
 
-// export const userProfileRouter = Router();
+export const userProfileRouter = Router();
+
+userProfileRouter.post(
+    "/users/userProfile",
+    remoteUpload.fields([
+      { name: "profilePicture", maxCount: 1 },
+      { name: "resume", maxCount: 1 },
+    ]),
+    checkUserSession,
+    createUserProfile
+  );
+  userProfileRouter.patch(
+      "/users/userProfile/:id",
+      remoteUpload.fields([
+        { name: "profilePicture", maxCount: 1 },
+        { name: "resume", maxCount: 1 },
+      ]),
+      checkUserSession,
+      updateUserProfile
+    );
+
+    userProfileRouter.get('/users/userProfile', getUserProfile);
+  
 
 
 
 
 // userProfileRouter.post('/userProfiles', upload.single('profilePicture'), checkUserSession,postuserProfile);
-// userProfileRouter.get('/userProfiles', getUserProfile);
+
 // userProfileRouter.get('/userProfiles/:id', getUserProfileById);
 // userProfileRouter.patch('/userProfiles/:id', upload.single('profilePicture'), updateUserProfile);
 // userProfileRouter.delete('/userProfiles/:id', deleteUserProfile);
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,32 +1,12 @@
 import {Router} from "express";
-import { getUser, getUsers, login, signup } from "../controllers/user_controller.js";
-import { getUserProfile, createUserProfile, updateUserProfile } from "../controllers/user-profile-controller.js";
+import { getUser, getUsers, login, logout, signup } from "../controllers/user_controller.js";
 import { checkUserSession } from "../middlewares/auth.js";
-import { remoteUpload } from "../middlewares/uploads.js";
 
 
 export const userRouter = Router();
 
 userRouter.post('/user/signup', signup);
 userRouter.post('/user/login', login);
+userRouter.post('/user/logout', checkUserSession, logout)
 userRouter.get("/users/auth/:userName", getUser);
-userRouter.get( "/users/userProfile", getUserProfile);
-
-userRouter.post(
-  "/users/userProfile",
-  remoteUpload.fields([
-    { name: "profilePicture", maxCount: 1 },
-    { name: "resume", maxCount: 1 },
-  ]),
-  checkUserSession,
-  createUserProfile
-);
-userRouter.patch(
-    "/users/userProfile/:id",
-    remoteUpload.fields([
-      { name: "profilePicture", maxCount: 1 },
-      { name: "resume", maxCount: 1 },
-    ]),
-    checkUserSession,
-    updateUserProfile
-  );
+userRouter.get("/users",getUsers );
