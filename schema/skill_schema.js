af20af25271d7b336e9f432fd789814d81f1e6c7
Ellen Ntiamoah <ellenntiamoah@gmail.com>
2024-07-18 13:02:11 +0000

    updated skills model and schema

--- schema/skill_schema.js
+++ schema/skill_schema.js
@@ -4,7 +4,7 @@ import Joi from "joi";
 export const skillSchema = Joi.object({
 
     skillName: Joi.string().required(),
-    levelOfProficiency: Joi.string().valid('beginner', 'intermediate', 'advanced', 'expert').required(),
+    levelOfProficiency: Joi.string().valid('25', '50', '75', '100').required(),
     user: Joi.string()
 
     });
